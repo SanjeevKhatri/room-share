6f33eda1a55518b73a6256e3dd812e05cd70a020
sanjeevkhatri <sanjeevkhatri29@gmail.com>
2021-02-12 17:17:08 -0600

    added room share service initial setup with dummy endpoint

---
+++ room-share-service/src/main/java/com/roomshare/RoomShareApplication.java
@@ -0,0 +1,11 @@
+package com.roomshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomShareApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RoomShareApplication.class, args);
+    }
+}
